24086214dac20413b260c9f492d304772f4075f4
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-10-17 10:33:04 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -3,6 +3,6 @@ var takesan='world';
 function sayhello65(){alert('Hello,' + who65 + '!');}
 function taro65(){who65 = '太郎';}
 function hanako65(){who65 = '花子';}
-function sayhello66(){alert('Hello,' + takesan + '!');}
-function taro66(){alert('太郎が来た') takesan = '太郎';}
-function hanako66(){alert('花子が来た') takesan = '花子';}
+
+function taro66(){alert('太郎' + 'が来た');}
+function hanako66(){alert('花子' + 'が来た');}
