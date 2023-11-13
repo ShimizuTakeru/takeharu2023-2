bc8dffee5db5e9d5655c0d8bac7ed449f4ef8056
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-11-13 21:01:07 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,4 +1,7 @@
 //画像が変更する
 function hyoji14(){document.getElementById('logo1').src='./image/saitama02.png';}
 function hyoji14(){
-  //画像が元に戻る
+  //setTimeout()を含む関数を呼び出す
+  setTimeout("hyoji15()",3000);}
+//画像が戻る
+function hyoji15(){document.getElementById('logo1').src='./image/saitama01.png';}
