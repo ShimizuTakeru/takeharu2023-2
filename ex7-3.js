114b98071df812ddcb254d27d86d01d39e827d7a
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-11-21 00:39:28 +0900

    Create ex7-3.js

---
+++ ex7-3.js
@@ -0,0 +1,7 @@
+function BMI(){
+  var x=document.getElementById('sintyo').value;
+  var y=document.getElementById('taiju').value;
+  
+  //BMIの求め方：BMI=体重[kg]÷（身長×身長）[m]
+  alert(Number(y)/(Number(x)*Number(x)));
+}
