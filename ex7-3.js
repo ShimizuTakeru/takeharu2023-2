2130eb61cd04f8dcfb7c0ec6bcb331a961695067
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-11-21 00:50:32 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -3,5 +3,5 @@ function BMI(){
   var y=document.getElementById('taiju').value;
   
   //BMIの求め方：BMI=体重[kg]÷（身長×身長）[m]
-  alert(Number(y)/(Number(x)*Number(x)));
+  alert(Number(y)/((Number(x)*100)*(Number(x)*100)));
 }
