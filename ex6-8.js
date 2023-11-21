b872531344b8d9d7bc9597bcedde73227e82c6b0
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-11-21 12:06:05 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -2,4 +2,4 @@ function sayhello68(){alert('Hello,'+document.getElementById('who68').innerHTML+
 function taro68(){document.getElementById('who68').innerHTML='太郎';}
 function hanako68(){document.getElementById('who68').innerHTML='花子';}
 
-function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/ノヴァ爆発.png" alt="やったぜ" class="imagechange3">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/ノヴァ爆発.png" alt="やったぜ">';}
