f3a26253e2dbbb178121aa557c79534be6cc268c
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-11-28 10:40:45 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -11,9 +11,9 @@ function BMI710(){
   //}
   //BMIの求め方：BMI=体重[kg]÷（身長×身長）[m]
   if(Number(y)/((Number(x)/100)*(Number(x)/100))<18.5);{
-    alert('あなたのBMIは+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+です。この体重は「低体重」となります。');}
+    alert('あなたのBMIは'+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+'です。この体重は「低体重」となります。');}
   else if(Number(y)/((Number(x)/100)*(Number(x)/100))<25);{
-    alert('あなたのBMIは+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+です。この体重は「普通」となります。');}
+    alert('あなたのBMIは'+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+'です。この体重は「普通」となります。');}
   else if(Number(y)/((Number(x)/100)*(Number(x)/100))>=25);{
-    alert('あなたのBMIは+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+です。この体重は「肥満」となります。');}
+    alert('あなたのBMIは'+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+'です。この体重は「肥満」となります。');}
 }
