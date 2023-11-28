65c20a980704e7802bfafcbbc2da3f65a7870b0e
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-11-28 10:08:22 +0900

    Update ex7-9.js

--- ex7-9.js
+++ ex7-9.js
@@ -1,18 +1,19 @@
 function nazo79(){
   if(document.getElementById('answer79').value=='夢'){
-    alert('あたり！');}
-    elseif(document.getElementById('answer79').value=='ゆめ'){
-      alert('あたり！');}
-    elseif(document.getElementById('answer79').value=='ユメ'){
-      alert('あたり！');}
-    elseif(document.getElementById('answer79').value=='yume'){
-      alert('あたり！');}
-    elseif(document.getElementById('answer79').value=='YUME'){
-      alert('あたり！');}
-    elseif(document.getElementById('answer79').value=='dream'){
-      alert('あたり！');}
-    elseif(document.getElementById('answer79').value=='DREAM'){
-      alert('あたり！');}
+    alert('あたり！');
+  }elseif(document.getElementById('answer79').value=='ゆめ'){
+    alert('あたり！');
+  }elseif(document.getElementById('answer79').value=='ユメ'){
+    alert('あたり！');
+  }elseif(document.getElementById('answer79').value=='yume'){
+    alert('あたり！');
+  }elseif(document.getElementById('answer79').value=='YUME'){
+    alert('あたり！');
+  }elseif(document.getElementById('answer79').value=='dream'){
+    alert('あたり！');
+  }elseif(document.getElementById('answer79').value=='DREAM'){
+    alert('あたり！');
+  }
   else{
     alert('はずれ！');}
   }
