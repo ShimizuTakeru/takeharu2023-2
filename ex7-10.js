015f81e42f73e636bdbdfc40bcdde08df093b229
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-12-12 10:19:41 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,11 +1,11 @@
 function BMI710(){
   var x=document.getElementById('sintyo1').value;
   var y=document.getElementById('taiju1').value;
-  var BMI2=(Number(y)/((Number(x)/100)*(Number(x)/100)));
+  var z=(Number(y)/((Number(x)/100)*(Number(x)/100)));
   
-  if(Number(BMI2)<18.5){alert('あなたのBMIは'+(Number(BMI2))+'です。この体重は「低体重」となります。');}
-  else if(Number(BMI2)<25){alert('あなたのBMIは'+(Number(BMI2))+'です。この体重は「普通」となります。');}
-  else{alert('あなたのBMIは'+(Number(BMI2))+'です。この体重は「肥満」となります。');}
+  if(Number(z)<18.5){alert('あなたのBMIは'+(Number(z))+'です。この体重は「低体重」となります。');}
+  else if(Number(z)<25){alert('あなたのBMIは'+(Number(z))+'です。この体重は「普通」となります。');}
+  else{alert('あなたのBMIは'+(Number(z))+'です。この体重は「肥満」となります。');}
 }
 
   //alert('あなたのBMIは+'Number(BMI2)'+です。この体重は'+'「'+'classif'+'」'+'となります。');}
