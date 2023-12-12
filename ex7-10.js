4fbea1eea48f7b8c13ed0201150c8e452ffe160c
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-12-12 09:57:55 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -1,6 +1,14 @@
 function BMI710(){
   var x=document.getElementById('sintyo1').value;
   var y=document.getElementById('taiju1').value;
+  var BMI2=Number(y)/((Number(x)/100)*(Number(x)/100));
+
+  var classif;
+  if(BMI2<18.5){classif='低体重';}
+  else if(BMI2<25){classif='普通';}
+  else{classif='肥満';}
+
+  alert('あなたのBMIは'+'BMI2'+'です。この体重は'+'「classif」'+'となります。');}
   
   //if(document.getElementById('answer79').value=='夢'){
     //alert('あたり！');}
@@ -10,10 +18,3 @@ function BMI710(){
     //alert('はずれ！');}
   //}
   //BMIの求め方：BMI=体重[kg]÷（身長×身長）[m]
-  if(Number(y)/((Number(x)/100)*(Number(x)/100))<18.5);{
-    alert('あなたのBMIは'+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+'です。この体重は「低体重」となります。');}
-  else if(Number(y)/((Number(x)/100)*(Number(x)/100))<25);{
-    alert('あなたのBMIは'+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+'です。この体重は「普通」となります。');}
-  else if(Number(y)/((Number(x)/100)*(Number(x)/100))>=25);{
-    alert('あなたのBMIは'+'(Number(y)/((Number(x)/100)*(Number(x)/100)))'+'です。この体重は「肥満」となります。');}
-}
