defcb77ad5cc88625590301bcd30e71bbb23a5d6
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-12-15 13:02:22 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,1 +1,12 @@
 
+
+
+//function BMI710(){
+//  var a=document.getElementById('sintyo1').value;
+//  var b=document.getElementById('taiju1').value;
+//  var z=(Number(b)/((Number(a)/100)*(Number(a)/100)));
+  
+//  if(Number(z)<18.5){alert('あなたのBMIは'+(Number(z))+'です。この体重は「低体重」となります。');}
+//  else if(Number(z)<25){alert('あなたのBMIは'+(Number(z))+'です。この体重は「普通」となります。');}
+//  else{alert('あなたのBMIは'+(Number(z))+'です。この体重は「肥満」となります。');}
+//}
