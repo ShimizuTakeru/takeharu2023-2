2a5e71eb061237d8a605a1e08ad56711fcdd754d
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-12-19 03:37:22 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,12 +1,10 @@
 function uru711(){
   var a=document.getElementById('seireki').value;
   
-  if(Number(a)/4){alert(Number(a)+'は閏年です。');}
-    else if(Number(a)/100 && Number(a)/400){alert(Number(a)+'は閏年です。');}
-      else if(Number(a)/100){alert(Number(a)+'は平年です。');}
-  else{alert(Number(a)+'は平年です。');}
+  if((Number(a)%4==0 && Number(a)%100!=0) || Number(a)%400==0){alert(Number(a)+'年は閏年です。');}
+  else{alert(Number(a)+'年は平年です。');}
 }
 
 //閏年、平年の定義
-//閏年：西暦が4で割り切れる年、西暦が100でも400でも割り切れる年
+//閏年：西暦が4で割り切れる年、西暦が400でも割り切れる年
 //平年：西暦が100で割り切れるが400で割り切れない年
