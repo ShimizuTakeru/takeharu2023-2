64d3731ef14e7949cc4bed77ecc53aa7fb445ab7
ShimizuTakeru <131091898+ShimizuTakeru@users.noreply.github.com>
2023-12-19 10:26:27 +0900

    Update ex7-10.js

--- ex7-10.js
+++ ex7-10.js
@@ -7,15 +7,3 @@ function BMI710(){
   else if(Number(z)<25){alert('あなたのBMIは'+(Number(z))+'です。この体重は「普通」となります。');}
   else{alert('あなたのBMIは'+(Number(z))+'です。この体重は「肥満」となります。');}
 }
-
-  //alert('あなたのBMIは+'Number(BMI2)'+です。この体重は'+'「'+'classif'+'」'+'となります。');}
-  
-  //if(document.getElementById('answer79').value=='夢'){
-    //alert('あたり！');}
-  //else if(document.getElementById('answer79').value=='ゆめ'){
-    //alert('あたり！');}
-  //else{
-    //alert('はずれ！');}
-  //}
-  //BMIの求め方：BMI=体重[kg]÷（身長×身長）[m]
-//alert(Number(y)/((Number(x)/100)*(Number(x)/100)));
